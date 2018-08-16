14a86b7b48273a0aeca182b659c62ba56ba15649
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-08-16 14:59:05 +0100

    added for loop to create and append blocks

--- blockchain.py
+++ blockchain.py
@@ -11,3 +11,12 @@ block_chain = [Block.create_genesis_block()] # initialising with the genesis blo
 # print hash of genesis
 print("hash: {}".format(block_chain[-1].hash()))
 
+# var for blockchain len
+num_blocks_to_add = 10
+
+# for loop for adding blocks to the list
+for i in range(1, num_blocks_to_add+1):
+    # add block with previous block hash
+    block_chain.append(Block((block_chain[-1]).hash(), "data", datetime.datetime.now()))
+    print("Block #{} created.".format(i))
+    print("Block #{} hash: {}".format(i, block_chain[i].hash()))
