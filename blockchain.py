fec457ed02357edc0b595cc16797021f50df0526
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-08-16 14:58:51 +0100

    created genesis block

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,13 @@
 # written by zachmolony 16/08/18
 
+import datetime
+
 #import the class from block.py
 from block import Block
 
-block_chain = [Block.create_genesis_block()]
+# creating linked list as the chain of blocks
+block_chain = [Block.create_genesis_block()] # initialising with the genesis block
+
+# print hash of genesis
+print("hash: {}".format(block_chain[-1].hash()))
+
