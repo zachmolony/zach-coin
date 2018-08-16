abebfe09c8062f1acfcf7c6f7e8f4adbd9ba407f
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-08-16 01:40:27 +0100

    added hashing method

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
 # written by zachmolony 16/08/18
 
+import hashlib
 
 class Block:
     def __init__(self, previous_block_hash, data, timestamp):
@@ -8,3 +9,14 @@ class Block:
         self.timestamp = timestamp
         self.hash = self.get_hash
 
+    # get hash method which hashes entire Block info
+    def get_hash(self):
+        # get binary representation of the header
+        header.binary = (str(self.previous_block_hash) +
+                        str(self.data) +
+                        str(self.timestamp)).encode()
+
+        # run through encrpyion twice to ensure that the hash is not changed
+        inner_hash = hashlib.sha256(header_binary).hexdigest().encode()
+        outer_hash = hashlib.sha256(inner_hash).hexdigest()
+        return outer_hash
