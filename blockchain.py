3e1b97b760547019351a7adbcb7865c2ab2ab4ce
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-08-16 01:39:57 +0100

    created block class

---
+++ blockchain.py
@@ -0,0 +1,10 @@
+# written by zachmolony 16/08/18
+
+
+class Block:
+    def __init__(self, previous_block_hash, data, timestamp):
+        self.previous_block_hash = previous_block_hash
+        self.data = data
+        self.timestamp = timestamp
+        self.hash = self.get_hash
+
